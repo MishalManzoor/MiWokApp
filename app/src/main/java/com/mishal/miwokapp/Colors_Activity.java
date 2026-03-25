a37160b1f1d6c1bbc8f30357039ee352ccf519b6
Mishal <97946979+MishalManzoor@users.noreply.github.com>
2026-03-25 19:13:21 +0500

    Update Colors_Activity.java

--- app/src/main/java/com/mishal/miwokapp/Colors_Activity.java
+++ app/src/main/java/com/mishal/miwokapp/Colors_Activity.java
@@ -79,22 +79,7 @@ public class Colors_Activity extends AppCompatActivity {
     }
 
     public void addDataInList(ArrayList<word> words){
-        words.add(new word("red", "weṭeṭṭi", R.drawable.color_red,
-                R.raw.color_red));
-        words.add(new word("green", "chokokki", R.drawable.color_green,
-                R.raw.color_green));
-        words.add(new word("brown", "ṭakaakki", R.drawable.color_brown,
-                R.raw.color_brown));
-        words.add(new word("gray", "ṭopoppi", R.drawable.color_gray,
-                R.raw.color_gray));
-        words.add(new word("black", "kululli", R.drawable.color_black,
-                R.raw.color_black));
-        words.add(new word("white", "kelelli", R.drawable.color_white,
-                R.raw.color_white));
-        words.add(new word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow,
-                R.raw.color_dusty_yellow));
-        words.add(new word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow,
-                R.raw.color_mustard_yellow));
+        /*...*/
     }
 
     public void showInAdapter(ArrayList<word> words){
@@ -108,16 +93,7 @@ public class Colors_Activity extends AppCompatActivity {
             word word = words.get(position);
 
             //media will run when audio focus will REQUEST_GRANTED
-            int result = audioManager.requestAudioFocus(listener, AudioManager.STREAM_MUSIC,
-                    AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-
-                mediaPlayer = MediaPlayer.create(Colors_Activity.this, word.getAudio());
-                mediaPlayer.start();
-
-                mediaPlayer.setOnCompletionListener(onCompletionListener);
-            }
+           /*...*/
         });
     }
 
